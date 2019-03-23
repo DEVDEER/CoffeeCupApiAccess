afef450553af2606f234bef14beaead5f78e9afd
Alexander Schmidt <alexander.schmidt@devdeer.com>
2019-03-23 21:26:27 +0100

    Added task-related models

---
+++ Logic/Logic.Models/ResponseModels/TasksResponseModel.cs
@@ -0,0 +1,25 @@
+﻿namespace devdeer.CoffeeCupApiAccess.Logic.Models.ResponseModels
+{
+    using System;
+    using System.Linq;
+
+    using Newtonsoft.Json;
+
+    using TransportModels;
+
+    /// <summary>
+    /// Represents the response of the CoffeeCup API when the tasks endpoint is called.
+    /// </summary>
+    public class TasksResponseModel
+    {
+        #region properties
+
+        /// <summary>
+        /// The list of user information.
+        /// </summary>
+        [JsonProperty("tasks")]
+        public TaskTransportModel[] Tasks { get; set; }
+
+        #endregion
+    }
+}
---
+++ Logic/Logic.Models/TransportModels/TaskTransportModel.cs
@@ -0,0 +1,38 @@
+﻿namespace devdeer.CoffeeCupApiAccess.Logic.Models.TransportModels
+{
+    using System;
+    using System.Linq;
+
+    using Newtonsoft.Json;
+
+    public class TaskTransportModel
+    {
+        #region properties
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("color")]
+        public int Color { get; set; }
+
+        [JsonProperty("hourlyRate")]
+        public double HourlyRate { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("billable")]
+        public bool IsBillable { get; set; }
+
+        [JsonProperty("favorite")]
+        public bool IsFavorite { get; set; }
+
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
+        [JsonProperty("status")]
+        public int Status { get; set; }
+
+        #endregion
+    }
+}
