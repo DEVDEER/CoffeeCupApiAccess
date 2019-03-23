6f80ebb17e27a9b96b13c6ac4ffdfab305f1ea4a
Alexander Schmidt <alexander.schmidt@devdeer.com>
2019-03-23 21:28:59 +0100

    Added task-assignment-related models

---
+++ Logic/Logic.Models/TransportModels/TaskAssignmentTransportModel.cs
@@ -0,0 +1,35 @@
+﻿namespace devdeer.CoffeeCupApiAccess.Logic.Models.TransportModels
+{
+    using System;
+    using System.Linq;
+
+    using Newtonsoft.Json;
+
+    public class TaskAssignmentTransportModel
+    {
+        #region properties
+
+        [JsonProperty("budgetHours")]
+        public int BudgetHours { get; set; }
+
+        [JsonProperty("hourlyRate")]
+        public double HourlyRate { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("billable")]
+        public bool IsBillable { get; set; }
+
+        [JsonProperty("project")]
+        public int Project { get; set; }
+
+        [JsonProperty("status")]
+        public int Status { get; set; }
+
+        [JsonProperty("task")]
+        public int Task { get; set; }
+
+        #endregion
+    }
+}
