8b9b93d5a91f9ad916db3cd8e49fef3705ad1b24
Alexander Schmidt <alexander.schmidt@devdeer.com>
2019-03-23 21:41:16 +0100

    Added user-employment-related models

---
+++ Logic/Logic.Models/ResponseModels/UserEmploymentsResponseModel.cs
@@ -0,0 +1,25 @@
+﻿namespace devdeer.CoffeeCupApiAccess.Logic.Models.ResponseModels
+{
+    using System;
+    using System.Linq;
+
+    using Newtonsoft.Json;
+
+    using TransportModels;
+
+    /// <summary>
+    /// Represents the response of the CoffeeCup API when the user employments endpoint is called.
+    /// </summary>
+    public class UserEmploymentsResponseModel : BaseResponseModel
+    {
+        #region properties
+
+        /// <summary>
+        /// The list of user employments.
+        /// </summary>
+        [JsonProperty("userEmployments")]
+        public UserEmploymentTransportModel[] UserEmployments { get; set; }
+
+        #endregion
+    }
+}
---
+++ Logic/Logic.Models/TransportModels/UserEmploymentTransportModel.cs
@@ -0,0 +1,62 @@
+﻿namespace devdeer.CoffeeCupApiAccess.Logic.Models.TransportModels
+{
+    using System;
+    using System.Linq;
+
+    using Newtonsoft.Json;
+
+    public class UserEmploymentTransportModel
+    {
+        #region properties
+
+        [JsonProperty("employerContribution")]
+        public double? EmployerContribution { get; set; }
+
+        [JsonProperty("employmentType")]
+        public int EmploymentType { get; set; }
+
+        [JsonProperty("hourlyRemuneration")]
+        public double? HourlyRemuneration { get; set; }
+
+        [JsonProperty("hoursFriday")]
+        public int? HoursFriday { get; set; }
+
+        [JsonProperty("hoursMonday")]
+        public int? HoursMonday { get; set; }
+
+        [JsonProperty("hoursSaturday")]
+        public int? HoursSaturday { get; set; }
+
+        [JsonProperty("hoursSunday")]
+        public int? HoursSunday { get; set; }
+
+        [JsonProperty("hoursThursday")]
+        public int? HoursThursday { get; set; }
+
+        [JsonProperty("hoursTuesday")]
+        public int? HoursTuesday { get; set; }
+
+        [JsonProperty("hoursWednesday")]
+        public int? HoursWednesday { get; set; }
+
+        [JsonProperty("hoursWeekly")]
+        public int? HoursWeekly { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("monthlyRemuneration")]
+        public double? MonthlyRemuneration { get; set; }
+
+        [JsonProperty("user")]
+        public int User { get; set; }
+
+        [JsonProperty("validFrom")]
+        public DateTime? ValidFrom { get; set; }
+
+        [JsonProperty("workHoursType")]
+        public int WorkHoursType { get; set; }
+
+        #endregion
+    }
+}
