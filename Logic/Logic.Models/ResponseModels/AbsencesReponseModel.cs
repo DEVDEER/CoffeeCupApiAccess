23fa1276efa4264bb25cad8c1ead8798ebf0348a
Alexander Schmidt <alexander.schmidt@devdeer.com>
2019-03-25 19:32:10 +0100

    Added absence models.

---
+++ Logic/Logic.Models/ResponseModels/AbsencesReponseModel.cs
@@ -0,0 +1,25 @@
+﻿namespace devdeer.CoffeeCupApiAccess.Logic.Models.ResponseModels
+{
+    using System;
+    using System.Linq;
+
+    using Newtonsoft.Json;
+
+    using TransportModels;
+
+    /// <summary>
+    /// Represents the response of the CoffeeCup API when the absences-endpoint is called.
+    /// </summary>
+    public class AbsencesReponseModel : BaseResponseModel
+    {
+        #region properties
+
+        /// <summary>
+        /// The list of absences.
+        /// </summary>
+        [JsonProperty("absences")]
+        public AbsenceTransportModel[] Absences { get; set; }
+
+        #endregion
+    }
+}
---
+++ Logic/Logic.Models/TransportModels/AbsenceTransportModel.cs
@@ -0,0 +1,56 @@
+﻿namespace devdeer.CoffeeCupApiAccess.Logic.Models.TransportModels
+{
+    using System;
+    using System.Linq;
+
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Contains the information about a single absence in CoffeeCup.
+    /// </summary>
+    public class AbsenceTransportModel
+    {
+        #region properties
+
+        [JsonProperty("absenceType")]
+        public int AbsenceTypeId { get; set; }
+
+        [JsonProperty("comment")]
+        public string Comment { get; set; }
+
+        [JsonProperty("countsAsVacation")]
+        public bool CountsAsVacation { get; set; }
+
+        [JsonProperty("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [JsonProperty("endDate")]
+        public DateTime? EndDate { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("remunerationActive")]
+        public bool IsRemunerationActive { get; set; }
+
+        [JsonProperty("rangeType")]
+        public string RangeType { get; set; }
+
+        [JsonProperty("reducesOvertime")]
+        public bool ReducesOvertime { get; set; }
+
+        [JsonProperty("startDate")]
+        public DateTime StartDate { get; set; }
+
+        [JsonProperty("updatedAt")]
+        public DateTime UpdatedAt { get; set; }
+
+        [JsonProperty("user")]
+        public int UserId { get; set; }
+
+        [JsonProperty("workHoursExpected")]
+        public bool WorkHoursExpected { get; set; }
+
+        #endregion
+    }
+}
