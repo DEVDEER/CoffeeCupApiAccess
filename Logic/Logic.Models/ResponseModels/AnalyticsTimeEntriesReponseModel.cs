c7b7fcaf112a0423cacb636fb66dd6ed77a9dd6a
alexander.schmidt <alexander.schmidt@devdeer.com>
2019-12-05 17:14:57 +0100

    Added response models

---
+++ Logic/Logic.Models/ResponseModels/AnalyticsTimeEntriesReponseModel.cs
@@ -0,0 +1,24 @@
+﻿namespace devdeer.CoffeeCupApiAccess.Logic.Models.ResponseModels
+{
+    using System;
+    using System.Linq;
+    using System.Text.Json.Serialization;
+
+    using TransportModels.Analytics;
+
+    /// <summary>
+    /// Represents the response of the CoffeeCup API when the analytics-time-entries-endpoint is called.
+    /// </summary>
+    public class AnalyticsTimeEntriesResponseModel : BaseResponseModel
+    {
+        #region properties
+
+        /// <summary>
+        /// The list of time-entry information.
+        /// </summary>
+        [JsonPropertyName("analyticsTimeEntries")]
+        public TimeEntryAnalyticsTransportModel[] TimeEntries { get; set; }
+
+        #endregion
+    }
+}
---
+++ Logic/Logic.Models/ResponseModels/ProjectAnalyticsResponseModel.cs
@@ -0,0 +1,24 @@
+﻿namespace devdeer.CoffeeCupApiAccess.Logic.Models.ResponseModels
+{
+    using System;
+    using System.Linq;
+    using System.Text.Json.Serialization;
+
+    using TransportModels.Analytics;
+
+    /// <summary>
+    /// Represents the response of the CoffeeCup API when the analytics-project-endpoint is called.
+    /// </summary>
+    public class ProjectAnalyticsResponseModel : BaseResponseModel
+    {
+        #region properties
+
+        /// <summary>
+        /// The project data.
+        /// </summary>
+        [JsonPropertyName("analyticsProject")]
+        public ProjectAnalyticsTransportModel Project { get; set; }
+
+        #endregion
+    }
+}
