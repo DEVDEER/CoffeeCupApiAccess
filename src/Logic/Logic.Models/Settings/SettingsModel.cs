015529b31a1e4fc67793da30fb2617ae5f42db29
devdeer-alex <alexander.schmidt@devdeer.com>
2023-06-17 17:17:07 +0200

    Models contain settings model now too.

---
+++ src/Logic/Logic.Models/Settings/SettingsModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace devdeer.CoffeeCupApiAccess.Logic.Models.Settings
+{
+    using System;
+    using System.Linq;
+
+    public class SettingsModel
+    {
+        #region properties
+
+        public string CoffeeCupApiVersion { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string CoffeeCupUsername { get; set; }
+
+        public string CoffeeCupPassword { get; set; }
+
+        public string CoffeeCupOrigin { get; set; }
+
+        #endregion
+    }
+}
