1ebc346fb61d61bb58796abe2c6f47265259daa5
devdeer-alex <alexander.schmidt@devdeer.com>
2023-07-10 14:56:07 +0200

    Added `TimeEntriesRequest` to the models.

---
+++ src/Logic/Logic.Models/Requests/TimeEntriesRequest.cs
@@ -0,0 +1,40 @@
+﻿namespace devdeer.CoffeeCupApiAccess.Logic.Models.Requests
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Represents the options to filter time entry requests.
+    /// </summary>
+    public class TimeEntriesRequest
+    {
+        #region properties
+
+        /// <summary>
+        /// The date from which the time entries should be returned.
+        /// </summary>
+        public DateTime? From { get; set; }
+
+        /// <summary>
+        /// The date up to which the time entries should be returned.
+        /// </summary>
+        public DateTime? To { get; set; }
+
+        /// <summary>
+        /// A flag indicating if relations should be resolved.
+        /// </summary>
+        public bool ResolveRelations { get; set; }
+
+        /// <summary>
+        /// Optional id of a client for which to filter.
+        /// </summary>
+        public int? ClientFilterId { get; set; }
+
+        /// <summary>
+        /// Optional id of a project for which to filter.
+        /// </summary>
+        public int? ProjectFilterId { get; set; }
+
+        #endregion
+    }
+}
