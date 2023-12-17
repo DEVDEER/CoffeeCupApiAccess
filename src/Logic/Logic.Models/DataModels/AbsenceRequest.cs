edc49d96d8e2187efcbbb9c79aa13d32eab1b72e
devdeer-alex <alexander.schmidt@devdeer.com>
2023-12-17 12:46:20 +0100

    Added types for absence requests.

---
+++ src/Logic/Logic.Models/DataModels/AbsenceRequest.cs
@@ -0,0 +1,52 @@
+﻿namespace devdeer.CoffeeCupApiAccess.Logic.Models.DataModels
+{
+    using System;
+    using System.Linq;
+    using System.Text.Json.Serialization;
+
+    /// <summary>
+    /// Contains the information about a single absence request in CoffeeCup.
+    /// </summary>
+    public class AbsenceRequest
+    {
+        #region properties
+
+        [JsonPropertyName("absenceType")]
+        public int AbsenceTypeId { get; set; }
+
+        [JsonPropertyName("senderComment")]
+        public string SenderComment { get; set; } = default!;
+
+        [JsonPropertyName("receiverComment")]
+        public string? ReceiverComment { get; set; } = default!;
+
+        [JsonPropertyName("createdAt")]
+        public DateTime CreatedAt { get; set; } = default!;
+
+        [JsonPropertyName("endDate")]
+        public DateTime? EndDate { get; set; } = default!;
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("rangeType")]
+        public string RangeType { get; set; } = default!;
+
+        [JsonPropertyName("startDate")]
+        public DateTime StartDate { get; set; } = default!;
+
+        [JsonPropertyName("updatedAt")]
+        public DateTime UpdatedAt { get; set; } = default!;
+
+        [JsonPropertyName("state")]
+        public string State { get; set; } = default;
+
+        [JsonPropertyName("sender")]
+        public int SenderId { get; set; }
+
+        [JsonPropertyName("receiver")]
+        public int ReceiverId { get; set; }
+
+        #endregion
+    }
+}
